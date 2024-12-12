4ceebb802cec1bfe4adfc994828db2eb0bb148a0
Sabbir Poonawala <sabbir.poonawala@hotmail.com>
2024-12-12 17:20:07 +0530

    Developer 1 Changed code

--- App/src/com/mph/demo/MainClass.java
+++ App/src/com/mph/demo/MainClass.java
@@ -2,9 +2,9 @@ package com.mph.demo;
 
 public class MainClass {
 
-	//Inital Prototype v1.0
+	//Developer 1 made changes
 	public static void main(String[] args) {
-		System.out.println("--Skeleton--");
+		System.out.println("--Developer 1 changes--");
 
 	}
 
